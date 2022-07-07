475662fd87f6b048501742ce6a1c49d2e93c6d86
dayatznur <dayatznur@gmail.com>
2022-07-07 22:14:08 +0700

    export images assets

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+export default {
+  images: {
+    bottomArrow: require('./images/bottom-arrow.png'),
+    rightArrow: require('./images/right-arrow.png'),
+    search: require('./images/search.png'),
+    copy: require('./images/copy.png'),
+  },
+};
